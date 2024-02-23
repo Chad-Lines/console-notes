db170a66f284813d54c89e7de8e697596a226745
Chad Lines, MBA-IT <chadlines@outlook.com>
2024-02-23 02:29:35 -0500

    1.0.0.1 Initialization

---
+++ ConsoleNotes/Note.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleNotes;
+
+internal class Note
+{
+    // Note myNote = new Note("Note Title", "Note Content", myNotebook);
+
+    #region Properties
+    public Guid UID { get; private set; }
+    public string Title { get; set; }
+    public DateTime Created { get; private set; }
+    public DateTime LastModified { get; private set; }
+    public string[] Tags { get; set; }
+    public Notebook Notebook { get; set; }
+    public string Content { get; set; }
+    #endregion
+
+    #region Constructor
+    public Note(string title, Notebook notebook, string[] tags = null, string content = null)
+    {
+        UID = Guid.NewGuid();
+        Title = title;
+        Created = DateTime.Now;
+        LastModified = DateTime.Now;
+        Tags = tags;
+        Notebook = notebook;
+        Content = content;
+    }
+    #endregion
+}
---
+++ ConsoleNotes/Notebook.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleNotes;
+
+internal class Notebook
+{
+    // Notebook myNotebook = new Notebook("My Notebook");
+
+    #region Properties
+    public Guid UID { get; private set; }
+    public string Name { get; set; }    
+    public DateTime Created { get; private set; }
+    public DateTime LastModified { get; private set; }
+    public string[] Tags { get; set; }
+    public Note[] Notes { get; set; }
+    #endregion
+
+    #region Constructor
+    public Notebook(string name)
+    {
+        UID = Guid.NewGuid();
+        Name = name;
+        Created = DateTime.Now;
+        LastModified = DateTime.Now;
+        Notes = new Note[0];
+    }
+    #endregion
+
+
+}
